0aa6e50be701454a39058e52f0680758cf102ac2
AunAfzal <144684402+AunAfzal@users.noreply.github.com>
2024-06-28 12:31:38 +0500

    Revert "upload"
    This reverts commit 9521452e6e0d3b6d1b050fd237b52c01a264133b.

--- backend/db/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const db_conn= ()=>{
-    mongoose.connect(process.env.DB)
-  .then(() => console.log('Connected to db'));
-  
-}
-
-module.exports = db_conn;
